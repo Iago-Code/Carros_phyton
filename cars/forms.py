02061fabfbec939d034e71a8c9754d3996d0f8eb
Iago leal Do Nascimento <iago.l.nascimento@gmail.com>
2026-04-01 01:41:58 -0300

    melhorias

--- cars/forms.py
+++ cars/forms.py
@@ -1,32 +1,16 @@
-from django import forms  # type: ignore
-from cars.models import Brand, Car  # type: ignore
-from cars.models import Car  # type: ignore
+from django import forms
+from cars.models import Car
 
-
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(queryset=Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
-    
 class CarModelForm(forms.ModelForm):
 
-    def save(self):
-        car = Car(
-            model=self.cleaned_data['model'],
-            brand=self.cleaned_data['brand'],
-            factory_year=self.cleaned_data['factory_year'],
-            model_year=self.cleaned_data['model_year'],
-            plate=self.cleaned_data['plate'],
-            value=self.cleaned_data['value'],
-            photo=self.cleaned_data['photo']
-        )
-        car.save()
-        return car
     class Meta: 
         model = Car
         fields = '__all__'
 
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+
+        if value < 20000:
+            self.add_error('value', 'Valor mínimo do carro deve ser de R$20.000')
+
+        return value
