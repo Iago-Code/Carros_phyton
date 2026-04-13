3cf42e13c9b7495cf6494214130d59eca3da6cd2
Iago leal Do Nascimento <iago.l.nascimento@gmail.com>
2026-04-12 22:22:00 -0300

    Atualizações

--- cars/forms.py
+++ cars/forms.py
@@ -14,3 +14,10 @@ class CarModelForm(forms.ModelForm):
             self.add_error('value', 'Valor mínimo do carro deve ser de R$20.000')
 
         return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Ano de fabricação deve ser maior que 1975')
+        return factory_year
+        
